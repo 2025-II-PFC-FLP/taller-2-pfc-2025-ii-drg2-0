4766f6e0bbc47cdd9133dc87f0fe1fdd82bfaa3b
NicoNGB7 <nicolas.gonzalez.bolanos@correounivalle.edu.co>
2025-10-02 22:41:08 -0500

    Creación de la función gcomplemento que  devuelve el  conjunto difuso correspondiente a su complemento

--- app/src/main/scala/taller/ConjuntosDifusos.scala
+++ app/src/main/scala/taller/ConjuntosDifusos.scala
@@ -10,20 +10,24 @@ class ConjuntosDifusos {
     require(d >= 1, "d debe ser >= 1")
     require(e >= 1, "e debe ser >= 1")
 // grande retorna una función que toma un Int y devuelve un Double.
-    (n: Int) =>
+    (n: Int) => //Función anónima lamda que recibe int
       // Quitamos los casos negativos ya que no son números grandes
-      if (n <= 0) 0.0
+      if (n <= 0) 0.0 // Se devuelve 0.0 para n <= 0
       else {
         val form1 = n.toDouble / (n + d).toDouble // n/(n+d)
         val form2 = math.pow(form1, e.toDouble) // (n/(n+d))^e
-        // Clamp por intervalo [0,1] evita retornar ligeras desviaciones por redondeo
+        // Clamp por intervalo [0,1] evita retornar ligeras desviaciones por redondeo (Limites de intervalos maximo y minimo)
         if (form2.isNaN) 0.0 else math.max(0.0, math.min(1.0, form2))
       }
   }
   def complemento(c: ConjDifuso): ConjDifuso = {
-    // Implementacion de la funcion complemento
+    (n: Int) => //funcion anonima que 'complemento' retorna ya que n toma un entero y se calcula el grado de pertenencia
+      val per = c(n) //grado de pertenencia del elemento n al conjunto difuso c
+      // per podrá ser cualquier doble
+      val num1 = if (per.isNaN || per.isInfinite) 0.0 else math.max(0.0, math.min(1.0, per)) // Clamp a intervalo [0,1] (Limites de intervalos maximo y minimo)
+      1.0 - num1 // complemento se calcula como 1 - grado de pertenencia
   }
-  4
+
   def union(cd1: ConjDifuso, cd2: ConjDifuso): ConjDifuso = {
     // Implementacion de la funcion union
   }
