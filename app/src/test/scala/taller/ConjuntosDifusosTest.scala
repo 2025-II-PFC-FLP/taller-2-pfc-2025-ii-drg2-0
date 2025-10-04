45e3339afddb4045230e0afb3c870223c8492314
CamiloTr10 <camilo.trejos@correounivalle.edu.co>
2025-10-04 15:53:11 -0500

    Configurar archivo de tests para ConjuntosDifusos con clase base y helper approx

--- app/src/test/scala/taller/ConjuntosDifusosTest.scala
+++ app/src/test/scala/taller/ConjuntosDifusosTest.scala
@@ -1,5 +1,12 @@
 package taller
 
-class ConjuntosDifusosTest {
+import org.scalatest.funsuite.AnyFunSuite
 
-}
+class ConjuntosDifusosTest extends AnyFunSuite {
+  val cd = new ConjuntosDifusos
+  import cd._
+
+  // tolerancia para comparar doubles
+  private val eps = 1e-6
+  private def approx(a: Double, b: Double, eps: Double = 1e-6): Boolean =
+    math.abs(a - b) <= eps
